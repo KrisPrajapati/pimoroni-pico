e4b40347c2bd3ff1ce90e44e2c79a16bbc01de04
MV Karan <mvkaran@github.com>
2022-11-04 14:29:08 +0000

    Add new conversion function

--- micropython/examples/badger2040/micropython-builtins.cmake
+++ micropython/examples/badger2040/micropython-builtins.cmake
@@ -10,6 +10,18 @@ function (convert_image TARGET IMAGE)
     target_sources(${TARGET} INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/../modules/${IMAGE}.py)
 endfunction()
 
+function (convert_gh_qr TARGET IMAGE)
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/../modules/${IMAGE}.py
+        
+        COMMAND
+            cd ${CMAKE_CURRENT_LIST_DIR}/assets && python3 ../../../../examples/badger2040/image_converter/convert.py --qr_resize --out_dir ${CMAKE_CURRENT_BINARY_DIR}/../modules --py ${IMAGE}.png
+
+        DEPENDS ${CMAKE_CURRENT_LIST_DIR}/assets/${IMAGE}.png
+    )
+    target_sources(${TARGET} INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/../modules/${IMAGE}.py)
+endfunction()
+
 function (convert_raw TARGET SRC DST)
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DST}.py
@@ -38,7 +50,7 @@ endfunction()
 convert_image(usermod_badger2040 badge_image)
 convert_image(usermod_badger2040 badgerpunk)
 convert_image(usermod_badger2040 launchericons)
-convert_image(usermod_badger2040 gh_qrcode)
+convert_gh_qr(usermod_badger2040 gh_qrcode)
 
 convert_raw(usermod_badger2040 289-0-wind-in-the-willows-abridged.txt witw)
 convert_raw(usermod_badger2040 contribution_page_1.txt contribution_page_1)
